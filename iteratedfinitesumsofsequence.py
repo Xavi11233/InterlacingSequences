cfd097553946c401df9d7582a66508f6f826328d
Xavi11233 <159134334+Xavi11233@users.noreply.github.com>
2026-04-25 12:12:48 +0100

    Add sums_of_powers function and update interlacing_fib_sequences2
    Added a new function to calculate sums of powers based on interlacing Fibonacci sequences. Updated the interlacing_fib_sequences2 function to include an additional initial value.

--- iteratedfinitesumsofsequence.py
+++ iteratedfinitesumsofsequence.py
@@ -45,7 +45,7 @@ def first_r_terms_absolute_value_sequence(j, r):
 
 def interlacing_fib_sequences2(F_0, F_1, F_2, r):
     """
-    Given the initial values F_0 and F_1, generates the rth term of the sequence with recurrence relation the same as the 
+    Given the initial values F_0, F_1 and F_2, generates the rth term of the sequence with recurrence relation the same as the 
     3 dimension interlacing fib sequence.
     """
     if r == 0:
@@ -70,3 +70,20 @@ def first_r_terms_sequence2(F_0, F_1, F_2, r):
         F_r = interlacing_fib_sequences2(F_0, F_1, F_2, i)
         sequence.append(F_r)
     return sequence
+
+def sums_of_powers(j, n):
+    """
+    Given whether it is the \mathscr{F}_{r}^{(2, 1)} or \mathscr{F}_{r}^{(2, 2)} sequence (j=1 or j=2), and the power of r, 
+    calculates the sum to infinity of (\mathscr{F}_{r}^{(2, j)}) ** n via the formula.
+    """
+    if j == 1:
+        if n % 2 == 1:
+            summation = sum(((scipy.special.comb(n, k)) * (2 - (5) ** (- k) * interlacing_fib_sequences(1, (n - 2 * k))) / (1 + (5) ** (- n) - (5) ** (- k) * interlacing_fib_sequences(1, (n - 2 * k)))) for k in range(0, int(((n - 1) / 2) + 1)) if n % 2 == 1)
+        if n % 2 == 0:
+            summation = scipy.special.comb(n, n / 2) * (1) / (1 - (5) ** (- n / 2)) + sum(((scipy.special.comb(n, k)) * (2 - (5) ** (- k) * interlacing_fib_sequences(1, (n - 2 * k))) / (1 + (5) ** (- n) - (5) ** (- k) * interlacing_fib_sequences(1, (n - 2 * k)))) for k in range(0, int(n / 2)))
+    if j == 2:
+        if n % 2 == 1:
+            summation = math.sqrt(5) ** n * sum((((scipy.special.comb(n, k)) * (-5) ** (-k) * (1 / math.sqrt(5)) * interlacing_fib_sequences(2, n - 1 - 2 * k)) / (1 + 5 ** (-n) - 5 ** (-k) * interlacing_fib_sequences(1, n - 2 * k))) for k in range(0, int(((n - 1) / 2) + 1)))
+        if n % 2 == 0:
+            summation = math.sqrt(5) ** n * (scipy.special.comb(n, n / 2) * ((- 1) ** (n / 2)) * (5) ** (- n / 2) / (1 - (5) ** (- n / 2)) + sum((((scipy.special.comb(n, k)) * ((- 5) ** (- k) * interlacing_fib_sequences(1, n - 2 * k) - (- 1) ** k * 2 * (5) ** (- n))) / (1 + 5 ** (- n) - 5 ** (- k) * interlacing_fib_sequences(1, n - 2 * k))) for k in range(0, int(n / 2))))
+    return summation
